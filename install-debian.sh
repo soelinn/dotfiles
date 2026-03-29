709090676d08fccb43d0af58d2e0f37c8db8d5e5
Soe Tun <soelinn@gmail.com>
2026-03-29 15:50:58 -0400

    Configure NVIDIA driver to enable Wayland in GDM
    Add configuration for NVIDIA driver to enable Wayland support.

--- install-debian.sh
+++ install-debian.sh
@@ -9,3 +9,16 @@ sudo mkdir -p /var/lib/gdm3/.config && sudo cp .config/monitors.xml /var/lib/gdm
 
 # install the entire .config to $HOME
 cp ./.config ~/
+
+
+# https://wiki.debian.org/NvidiaGraphicsDrivers#Wayland_configuration
+#     In case of the NV proprietary driver, specific kernel module options need to be enabled
+#     and even then it may not function properly on certain hardware configurations
+#     (in which case Gnome will automatically fallback to X11).
+#
+# Sets proprietary NVIDIA driver's kernel module options to make Wayland option available in GDM login screen.
+echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1
+options nvidia-drm modeset=1
+options nvidia-drm fbdev=1" > ./nvidia-wayland.conf
+sudo chown root:root /nvidia-wayland.conf
+sudo mv ./nvidia-wayland.conf /etc/modprobe.d/nvidia-wayland.conf
